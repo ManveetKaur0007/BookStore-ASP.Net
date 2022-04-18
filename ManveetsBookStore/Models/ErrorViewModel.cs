b0364b14c29e0f66b55536d9e86c333cac4b51cf
Manveet Kaur <W0782002@myscc.ca>
2022-04-18 16:47:07 -0400

    new

---
+++ ManveetsBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ManveetsBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
