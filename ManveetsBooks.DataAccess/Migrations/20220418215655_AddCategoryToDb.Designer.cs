f9de7e689018c91300c74692f4a1107890949956
Manveet Kaur <W0782002@myscc.ca>
2022-04-18 17:57:14 -0400

    data

--- ManveetsBooks.DataAccess/Migrations/20220418215655_AddCategoryToDb.Designer.cs
+++ ManveetsBooks.DataAccess/Migrations/20220418215655_AddCategoryToDb.Designer.cs
@@ -4,14 +4,16 @@ using ManveetsBookStore.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ManveetsBooks.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220418215655_AddCategoryToDb")]
+    partial class AddCategoryToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
