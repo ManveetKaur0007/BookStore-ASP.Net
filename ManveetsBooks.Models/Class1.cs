3d9ff9d61031b708eb8a84391f36fd72990590d7
Manveet Kaur <W0782002@myscc.ca>
2022-04-18 17:07:58 -0400

    auto generated classes deleted

--- ManveetsBooks.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ManveetsBooks.Models
-{
-    public class Class1
-    {
-    }
-}
--- ManveetsBooks.Utility/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ManveetsBooks.Utility
-{
-    public class Class1
-    {
-    }
-}
