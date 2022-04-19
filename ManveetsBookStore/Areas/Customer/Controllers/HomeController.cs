4a203cbf7fcafcbc598100153d3c2c24bb78ebe1
Manveet Kaur <W0782002@myscc.ca>
2022-04-19 12:58:20 -0400

    area<customer<view

--- ManveetsBookStore/Areas/Customer/Controllers/HomeController.cs
+++ ManveetsBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using ManveetsBooks.Models;
+﻿using ManveetsBooks.DataAccess.Repository.IRepository;
+using ManveetsBooks.Models;
 using ManveetsBookStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -8,21 +9,24 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ManveetsBookStore.Area.Customer.Controllers
+namespace BulkyBook.Ares.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unifOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unifOfWork)
         {
             _logger = logger;
+            _unifOfWork = unifOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unifOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
