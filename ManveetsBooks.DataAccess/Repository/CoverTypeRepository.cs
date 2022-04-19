fceb689e384f43bd71ee99036614250efa62eefe
Manveet Kaur <W0782002@myscc.ca>
2022-04-19 00:06:27 -0400

    new cs and interface and migration

---
+++ ManveetsBooks.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,30 @@
+﻿using ManveetsBooks.DataAccess.Data;
+using ManveetsBooks.DataAccess.Repository.IRepository;
+using ManveetsBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManveetsBooks.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(CoverType coverType)
+        {
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = coverType.Name;
+            }
+        }
+    }
+}
